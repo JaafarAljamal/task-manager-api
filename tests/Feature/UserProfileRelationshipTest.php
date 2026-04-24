7122c32a5a83358e80405b112431565dda64db84
JaafarAljamal <jaafar.aljamal@gmail.com>
2026-04-25 01:41:03 +0300

    test: add failing test for user profile updating (Red Phase)

--- tests/Feature/UserProfileRelationshipTest.php
+++ tests/Feature/UserProfileRelationshipTest.php
@@ -74,4 +74,36 @@ class UserProfileRelationshipTest extends TestCase
         $response->assertStatus(200);
         $response->assertJsonFragment(['phone' => $profile->phone]);
     }
+
+    /**
+     * Test that a user can update the associated profile by user ID.
+     * 
+     * @return void
+     */
+    public function test_user_can_update_associated_profile(): void
+    {
+        // Arrange: Create a sample user, an associated profile, and new profile data
+        $user = User::factory()->create();
+        $profile = Profile::create([
+            'user_id' => $user->id,
+            'phone' => '123456789',
+            'address' => 'Test address',
+            'date_of_birth' => '2026-04-23',
+            'bio' => 'Software Engineer.'
+        ]);
+        $updateDate = [
+            'phone' => '0123456789',
+            'address' => 'New address',
+            'date_of_birth' => '2025-04-24',
+            'bio' => 'Back-End Developer.',
+        ];
+
+        // Act: Send PUT request with new data
+        $response = $this->put("/api/profile/{$user->id}", $updateDate);
+
+        // Assert: Check response status, content, and the profile in database
+        $response->assertStatus(200);
+        $response->assertJsonFragment(['phone' => '0123456789']);
+        $this->assertDatabaseHas('profiles', $updateDate);
+    }
 }
